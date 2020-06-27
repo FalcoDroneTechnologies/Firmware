ca1dd8b841dac16196f0dc3f15264a645925a1cb
JaeyoungLim <gokoreas@gmail.com>
2020-06-27 12:39:38 +0200

    Add library

--- src/modules/rover_pos_control/RoverPositionControl.hpp
+++ src/modules/rover_pos_control/RoverPositionControl.hpp
@@ -45,8 +45,8 @@
 
 #include <drivers/drv_hrt.h>
 #include <lib/ecl/geo/geo.h>
-#include <lib/ecl/l1/ecl_l1_pos_controller.h>
-#include <lib/ecl/attitude_fw/ecl_yaw_controller.h>
+#include <lib/l1/ECL_L1_Pos_Controller.hpp>
+// #include <lib/ecl/attitude_fw/ecl_yaw_controller.h>
 #include <lib/mathlib/mathlib.h>
 #include <lib/perf/perf_counter.h>
 #include <lib/pid/pid.h>
